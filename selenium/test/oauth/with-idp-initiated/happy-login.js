774c2bd8af8c18514e0394880da9741163eb5ef4
Codacy Archive <codacy-archive@local>
2025-12-22 11:13:37 +0000

    snapshot 2025-12-22T11:13:37.016258+00:00

--- selenium/test/oauth/with-idp-initiated/happy-login.js
+++ selenium/test/oauth/with-idp-initiated/happy-login.js
@@ -1,19 +1,22 @@
 const { By, Key, until, Builder } = require('selenium-webdriver')
 require('chromedriver')
 const assert = require('assert')
-const { buildDriver, goToLogin, goTo, tokenFor, captureScreensFor, teardown } = require('../../utils')
+const { buildDriver, captureScreensFor, teardown } = require('../../utils')
 
 const OverviewPage = require('../../pageobjects/OverviewPage')
 const FakePortalPage = require('../../pageobjects/FakePortalPage')
 
 describe('A user with a JWT token', function () {
+  let driver
   let overview
   let captureScreen
-  let token
   let fakePortal
-  let driver
+  let username
+  let password
 
   before(async function () {
+    username = process.env.MGT_CLIENT_ID_FOR_IDP_INITIATED || 'rabbit_idp_user'
+    password = process.env.MGT_CLIENT_SECRET_FOR_IDP_INITIATED || 'rabbit_idp_user'
     driver = buildDriver()
     overview = new OverviewPage(driver)
     captureScreen = captureScreensFor(driver, __filename)
@@ -21,13 +24,13 @@ describe('A user with a JWT token', function () {
   })
 
   it('can log in presenting the token to the /login URL via fakeportal', async function () {
-    await fakePortal.goToHome("rabbit_idp_user", "rabbit_idp_user")
+    await fakePortal.goToHome(username, password)
     if (!await fakePortal.isLoaded()) {
       throw new Error('Failed to load fakePortal')
     }
     await fakePortal.login()
     await overview.isLoaded()
-    assert.equal(await overview.getUser(), 'User rabbit_idp_user')
+    assert.equal(await overview.getUser(), 'User ' + username)
   })
 
   after(async function () {
